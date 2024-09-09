9bef4742b87a9150daaf726c5b78b1cf392caef2
phil <p@pkpowell.net>
2024-09-09 14:31:48 +0200

    updates

--- syncmap.go
+++ syncmap.go
@@ -5,27 +5,30 @@ import (
 	"sync"
 )
 
-type PointerMap[K PointerType[T], V PointerMapBoolType, T comparable] struct {
+type TypeType interface {
+	comparable
+}
+
+/////////////////////////////
+// PointerMap
+/////////////////////////////
+
+type PointerType[T TypeType] interface {
+	comparable
+	ID() string
+	Type() T
+}
+
+type PointerBoolType interface {
+	struct{}
+}
+
+type PointerMap[K PointerType[T], V PointerBoolType, T TypeType] struct {
 	mtx sync.RWMutex
 	m   map[K]V
 }
 
-type PointerMapBoolType interface {
-	comparable
-	struct{}
-	// ID() string
-	// Type() T
-	// All()
-}
-
-type PointerType[T comparable] interface {
-	comparable
-	ID() string
-	Type() T
-	All()
-}
-
-func NewPointerMap[K PointerType[T], T comparable]() PointerMap[K, struct{}, T] {
+func NewPointerMap[K PointerType[T], T TypeType]() PointerMap[K, struct{}, T] {
 	return PointerMap[K, struct{}, T]{
 		mtx: sync.RWMutex{},
 		m:   make(map[K]struct{}),
@@ -60,15 +63,6 @@ func (m *PointerMap[K, V, _]) Length() int {
 	return len(m.m)
 }
 
-// func (m *PointerMap[K, V, T]) OfType(t T) (k K) {
-// 	for k := range m.m {
-// 		if k.Type() == t {
-// 			return k
-// 		}
-// 	}
-// 	return k
-// }
-
 // All is an iterator over the elements of s.
 func (s *PointerMap[K, V, _]) All() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
@@ -80,7 +74,7 @@ func (s *PointerMap[K, V, _]) All() iter.Seq2[K, V] {
 	}
 }
 
-// All is an iterator over the elements of s.
+// OfType is an iterator over the elements of s with type t.
 func (s *PointerMap[K, V, T]) OfType(t T) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
 		for k, v := range s.m {
@@ -102,40 +96,21 @@ func (m *PointerMap[K, V, _]) GetByID(id string) (k K) {
 	return k
 }
 
-// // All is an iterator over the elements of s.
-// func (s *PointerMap[K, V, T]) ByType(t T) iter.Seq2[K, V] {
-// 	return func(yield func(K, V) bool) {
-// 		for k, v := range s.m {
-// 			if k.ObjType(t) == t {
-// 				if !yield(k, v) {
-// 					return
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+/////////////////////////////
+// KeyValMap
+/////////////////////////////
 
-// func(k *PointerType)ObjType(){
-
-// }
-
-type MapValue[T comparable] interface {
+type MapValue[T TypeType] interface {
 	comparable
 	ID() string
 	Type() T
-	Delete(string)
-	Add(string)
 }
 
-type MapKey[T comparable] interface {
+type MapKey interface {
 	comparable
-	ID() string
-	Type() T
-	Delete(string)
-	Add(string)
 }
 
-type KeyValMap[K MapKey[T], V MapValue[T], T comparable] struct {
+type KeyValMap[K MapKey, V MapValue[T], T TypeType] struct {
 	mtx sync.RWMutex
 	m   map[K]V
 }
@@ -145,12 +120,6 @@ func (m *KeyValMap[K, V, T]) Get(key K) V {
 	defer m.mtx.RUnlock()
 	return m.m[key]
 }
-
-// func (m *PointerMap[K, V]) Set(key K, value V) {
-// 	m.mtx.Lock()
-// 	m.m[key] = value
-// 	m.mtx.Unlock()
-// }
 
 func (m *KeyValMap[K, V, _]) Set(key K, value V) {
 	m.mtx.Lock()
@@ -180,11 +149,14 @@ func (s *KeyValMap[K, V, _]) All() iter.Seq2[K, V] {
 	}
 }
 
-func (m *KeyValMap[K, V, T]) OfType(t T) (k K) {
-	for k := range m.m {
-		if k.Type() == t {
-			return k
+func (s *KeyValMap[K, V, T]) OfType(t T) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range s.m {
+			if v.Type() == t {
+				if !yield(k, v) {
+					return
+				}
+			}
 		}
 	}
-	return k
 }
