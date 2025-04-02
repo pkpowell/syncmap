d0824e82809eb024a15899dede78ca00acd0d907
phil <p@pkpowell.net>
2025-04-02 11:29:32 +0200

    updates

--- syncmap.go
+++ syncmap.go
@@ -228,8 +228,8 @@ func (m *Collection[_, _]) LenStr() string {
 
 // All iterates over all elements of K
 func (m *Collection[K, V]) All() iter.Seq2[K, V] {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
 
 	return func(yield func(K, V) bool) {
 
