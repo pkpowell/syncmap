643dbaaa51a7c8b79ebc33e9971f0a09290ce0dc
phil <p@pkpowell.net>
2026-04-07 14:59:44 +0200

    clean up some pointers, remove GetAll() method

--- syncmap.go
+++ syncmap.go
@@ -29,9 +29,9 @@ type Collection[K MapKey, V MapValue] struct {
 // Mid-Stack Inlined ?
 // see https://dave.cheney.net/2020/05/02/mid-stack-inlining-in-go
 
-func NewCollection[K MapKey, V MapValue]() *Collection[K, V] {
-	var c Collection[K, V]
-	return newCollection(&c)
+func NewCollection[K MapKey, V MapValue]() (c *Collection[K, V]) {
+	// var c Collection[K, V]
+	return newCollection(c)
 }
 
 func newCollection[K MapKey, V MapValue](c *Collection[K, V]) *Collection[K, V] {
@@ -41,124 +41,125 @@ func newCollection[K MapKey, V MapValue](c *Collection[K, V]) *Collection[K, V] 
 }
 
 // Exists check if key exists
-func (m *Collection[K, _]) Exists(key K) (ok bool) {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[K, _]) Exists(key K) (ok bool) {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	_, ok = m.m[key]
-	return
+	_, ok = c.m[key]
+	return ok
 }
 
 // Get val with key
-func (m *Collection[K, V]) Get(key K) (val V, ok bool) {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[K, V]) Get(key K) (val V, ok bool) {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	val, ok = m.m[key]
-	return
+	val, ok = c.m[key]
+	return val, ok
 }
 
 // Get val with key and write to v
-func (m *Collection[K, V]) GetP(key K, v *V) (ok bool) {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[K, V]) GetP(key K, val *V) (ok bool) {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	*v, ok = m.m[key]
-	return
+	*val, ok = c.m[key]
+	return ok
 }
 
-// Get whole map - use ToMap
-func (m *Collection[K, V]) GetAll() (val *map[K]V) {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+// // Get whole map - use ToMap
+// func (c *Collection[K, V]) GetAll() (val *map[K]V) {
+// 	c.mtx.RLock()
+// 	defer c.mtx.RUnlock()
 
-	val = &m.m
-	return
-}
+// 	val = &c.m
+// 	return val
+// }
 
 // Get whole map - replaces GetAll
-func (m *Collection[K, V]) ToMap() (val map[K]V) {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[K, V]) ToMap() map[K]V {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	val = m.m
-	return
+	return c.m
+	// val = c.m
+	// return val
 }
 
 // Set / Overwrite map from map
-func (m *Collection[K, V]) Set(v map[K]V) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, V]) Set(v map[K]V) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	m.m = v
+	c.m = v
 }
 
 // Add key / val to map
-func (m *Collection[K, V]) Add(k K, v V) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, V]) Add(k K, v V) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	m.m[k] = v
+	c.m[k] = v
 }
 
 // Add key / val to map, returns 'updated'. Doesn't really work....
-func (m *Collection[K, V]) AddCompare(k K, v V) (updated bool) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, V]) AddCompare(k K, v V) (updated bool) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	updated = m.m[k] == v
+	updated = c.m[k] == v
 
-	m.m[k] = v
+	c.m[k] = v
 	return
 }
 
 // Remove key from map
-func (m *Collection[K, _]) Remove(key K) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, _]) Remove(key K) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	delete(m.m, key)
+	delete(c.m, key)
 }
 
 // Mark key as deleted
-func (m *Collection[K, _]) Delete(key K) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, _]) Delete(key K) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	m.m[key].Del(true)
+	c.m[key].Del(true)
 }
 
 // Mark key as not deleted
-func (m *Collection[K, _]) UnDelete(key K) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+func (c *Collection[K, _]) UnDelete(key K) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
-	m.m[key].Del(false)
+	c.m[key].Del(false)
 }
 
 // Len of map
-func (m *Collection[_, _]) Len() int {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[_, _]) Len() int {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	return len(m.m)
+	return len(c.m)
 }
 
-func (m *Collection[_, _]) LenStr() string {
-	m.mtx.RLock()
-	defer m.mtx.RUnlock()
+func (c *Collection[_, _]) LenStr() string {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 
-	return strconv.Itoa(len(m.m))
+	return strconv.Itoa(len(c.m))
 }
 
 // All iterates over all elements of K
 // Replaced by Iter()
-func (m *Collection[K, V]) All() iter.Seq2[K, V] {
+func (c *Collection[K, V]) All() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		m.mtx.RLock()
-		defer m.mtx.RUnlock()
+		c.mtx.RLock()
+		defer c.mtx.RUnlock()
 
-		for k, v := range m.m {
+		for k, v := range c.m {
 			if !yield(k, v) {
 				return
 			}
@@ -167,12 +168,12 @@ func (m *Collection[K, V]) All() iter.Seq2[K, V] {
 }
 
 // Iter iterates over all elements of K - replaces .All()
-func (m *Collection[K, V]) Iter() iter.Seq2[K, V] {
+func (c *Collection[K, V]) Iter() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		m.mtx.RLock()
-		defer m.mtx.RUnlock()
+		c.mtx.RLock()
+		defer c.mtx.RUnlock()
 
-		for k, v := range m.m {
+		for k, v := range c.m {
 			if !yield(k, v) {
 				return
 			}
